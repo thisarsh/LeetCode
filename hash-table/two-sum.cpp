564001a7a521e70dad92b50660d548f73b8c2b19
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-12-01 23:15:43 +0530

    LC-two-sum [Runtime: 9 ms; Memory: 16 MB]

--- hash-table/two-sum.cpp
+++ hash-table/two-sum.cpp
@@ -1,15 +1,46 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                if (nums[i] + nums[j]==target) {
-                    return {i, j};
-    }
+
+        int n=nums.size();
+        unordered_map <int,int> mpp;
+        for(int i=0; i<n; i++){
+            mpp[target-nums[i]]=i;
+        }
+        for(int i=0; i<n; i++){
+          
+          auto it=mpp.find(nums[i]);
+          if(it!=mpp.end() && it->second !=i)
+               { int index=it->second;
+                
+                return {i,index};
             }
         }
-                return {};
-            }
+
+
+
+
+
+
+
+
+        
+        // vector <int> hash(n,0);
+
+        // for (int i=0; i<n; i++){
+        //     hash[i]=target-nums[i];
+   
+        // }
+        // for(int i=0 ; i<n; i++){
+        //   auto it=  find(hash.begin(),hash.end(),nums[i]);
+        //   if(it!=hash.end()){
+        //     int index=it-hash.begin();
+        //     if(index!=i)
+        //     return{index,i};
             
+        //   }
+        //   }
+        
+        return {};
+    }
 };
