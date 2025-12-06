b481f03a8cd319ab973053b93613d990ed3037d3
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-12-06 15:40:13 +0530

    LC-valid-parenthesis-string [Runtime: N/A; Memory: N/A]

--- stack/valid-parenthesis-string.cpp
+++ stack/valid-parenthesis-string.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        int star=0,curr=0;
+        int star=0,curr=0,o=0,c=0;
         for(auto it:s){
             if(it=='(')curr++;
             else if(it==')')curr--;
             else star++;
         }
-        if(curr==0 || abs(curr)==star)return 1;
+        if(curr==0 || abs(curr)<=star)return 1;
        return 0;
     }
 };
