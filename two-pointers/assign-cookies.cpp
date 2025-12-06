281b0c93c0e5b59015474a39169e66d08cd4f240
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-12-06 14:40:16 +0530

    LC-assign-cookies [Runtime: 13 ms; Memory: 45 MB]

--- two-pointers/assign-cookies.cpp
+++ two-pointers/assign-cookies.cpp
@@ -4,15 +4,12 @@ public:
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
         int p1=0,p2=0,count=0,n1=g.size(),n2=s.size();
-        while(p1<n1 && p1<n2){
+        while(p1<n1 && p2<n2){
             if(s[p2]>=g[p1]){
                 count++;
                 p1++;
-                p2++;
             }
-            else {
-                p1++;
-            }
+            p2++;
         }
         return count;
 
