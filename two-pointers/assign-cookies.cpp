0fa2dfcb370efee695215934aaeebe6a50c9e44d
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-12-06 14:35:05 +0530

    LC-assign-cookies [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/assign-cookies.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int p1=0,p2=0,count=0,n1=g.size(),n2=s.size();
+        while(p1<n1 && p1<n2){
+            if(s[p2]>=g[p1]){
+                count++;
+                p1++;
+                p2++;
+            }
+            else {
+                p1++;
+            }
+        }
+        return count;
+
+    }
+};
