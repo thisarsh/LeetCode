70dac4dc22816be080605e4c35544af281ac9c92
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-12-12 22:15:10 +0530

    LC-kth-largest-element-in-an-array [Runtime: N/A; Memory: N/A]

--- sorting/kth-largest-element-in-an-array.cpp
+++ sorting/kth-largest-element-in-an-array.cpp
@@ -3,11 +3,10 @@ public:
     int findKthLargest(vector<int>& nums, int k) {
         int n = nums.size(), i = 0;
         sort(nums.begin(), nums.end(), greater<>());
-
-        while (i < n && k > 1) {
-            i++;
-            k--;
-        }
-        return nums[i];
+        // for(int it:nums){
+        //     i++;
+        //     if(i==k)break;
+        // }
+        return nums[n-k];
     }
 };
