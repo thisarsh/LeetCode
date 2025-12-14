2ccf41fe3ee8c86f256dd82f43e6349d33b73b90
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-12-14 13:06:08 +0530

    LC-number-of-ways-to-divide-a-long-corridor [Runtime: N/A; Memory: N/A]

---
+++ string/number-of-ways-to-divide-a-long-corridor.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numberOfWays(string corridor) {
+        int ans=0,total=0,sum=0;
+        for(auto it:corridor){
+            if(it=='S')sum++;
+        }
+        if(sum%2!=0 )return 0;
+
+        for(auto it:corridor){
+            if(it=='S')total++;
+            if(total>0 && total %2==0)ans++;
+        }
+        return ans-1;
+    }
+};
