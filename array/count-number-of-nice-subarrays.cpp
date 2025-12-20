1064c79795ac86392bda9ffcc4bcce07d3c34322
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-12-21 02:41:14 +0530

    LC-count-number-of-nice-subarrays [Runtime: 71 ms; Memory: 94.4 MB]

--- array/count-number-of-nice-subarrays.cpp
+++ array/count-number-of-nice-subarrays.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-      for(auto &it:nums){
-        if(it%2){
-            it=1;
-        }
-        else it=0;
-      }
-      int sum=0,ans=0;
-      unordered_map <int,int> mpp;
-      mpp[0]=1;
-      for(auto it: nums){
-        sum+=it;
-        auto j=mpp.find(sum-k);
-
-        if(j!=mpp.end()){
-            ans+=j->second;
+        int sum=0,ans=0;
+        unordered_map <int,int> mpp;
+        mpp[0]=1;
+    for (auto &i:nums){
+        if(i%2) i=1;
+        else i=0;
+        sum+=i;
+        if(mpp.count(sum-k)){
+            ans+=mpp[sum-k];
         }
         mpp[sum]++;
 
 
-      } 
-      return ans;
+    }return ans; 
     }
 };
