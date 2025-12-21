34c0a330082b8de9422fe2d7031a10806bc95338
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-12-21 17:31:58 +0530

    LC-longest-consecutive-sequence [Runtime: 86 ms; Memory: 89 MB]

--- union-find/longest-consecutive-sequence.cpp
+++ union-find/longest-consecutive-sequence.cpp
@@ -1,43 +1,19 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-    int ans=0,len=0;
-    unordered_set <int> st(nums.begin(),nums.end());
-    for(int i=0; i<nums.size(); i++){
-
-        if(!st.count(nums[i]-1)){
-            int curr=nums[i];
-            len=0;
-    
-            while(st.count(curr)){
-                curr++;
-                len++;
+        int ans=0,len=0;
+        unordered_set <int> st(nums.begin(),nums.end());
+        for(int i:st){
+            if(!st.count(i-1)){
+                int curr=i;
+                len=0;
+                while(st.count(curr)){
+                    len++;
+                    curr++;
+                }
             }
-
+            ans=max(ans,len);
         }
-        ans=max(len,ans);
-     
-    }
-return ans;
-
-
-        // int n=nums.size(),ans=1,temp=1;
-        // if(n==0)return 0;
-        // int mini=*min_element(nums.begin(),nums.end());
-        // map <int,int> mpp;
-        // for(auto it:nums){
-        //     mpp[it]++;
-        // }
-        // for(auto it:mpp){
-        //     if(mpp.find(it.first+1)!=mpp.end()){
-        //         temp++;
-        //         ans=max(temp,ans);
-                
-        //     }
-        //     else temp=1;
-
-        // }
-        // return ans;
+        return ans;
     }
 };
-// auto init = atexit([](){ofstream("display_runtime.txt")<<"0";});
