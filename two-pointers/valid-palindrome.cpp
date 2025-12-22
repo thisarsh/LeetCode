f7adaebaeb80f8d5ce58e6b3a38c50a968a3a8ad
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-12-23 03:51:24 +0530

    LC-valid-palindrome [Runtime: 2 ms; Memory: 9.9 MB]

--- two-pointers/valid-palindrome.cpp
+++ two-pointers/valid-palindrome.cpp
@@ -5,8 +5,8 @@ public:
         int l=0,r=n-1;
         while(l<r){
 
-           while(l<r && !((s[l]<='z' && s[l]>='a') || (s[l]<='Z' && s[l]>='A')) || ( s[l]>='0' && s[l]<='9')) l++;
-           while(l<r && !((s[r]<='z' && s[r]>='a' )||( s[r]<='Z' && s[r]>='A')) || ( s[r]>='0' && s[r]<='9')) r--;
+           while(l<r && !((s[l]<='z' && s[l]>='a') || (s[l]<='Z' && s[l]>='A') || ( s[l]>='0' && s[l]<='9'))) l++;
+           while(l<r && !((s[r]<='z' && s[r]>='a' )||( s[r]<='Z' && s[r]>='A') || ( s[r]>='0' && s[r]<='9'))) r--;
             
             if(tolower(s[l])!= tolower(s[r]))return 0;
             l++;
