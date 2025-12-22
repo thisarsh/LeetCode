78a01c43ef93ea675c8ec502281add52c0a9f58a
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-12-22 14:46:16 +0530

    LC-3sum [Runtime: 55 ms; Memory: 29 MB]

--- two-pointers/3sum.cpp
+++ two-pointers/3sum.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size(), tsum = 0, l = 1, r, ans = 0, i = 0;
-        r = n - 1;
-        vector<vector<int>> answer;
-        sort(nums.begin(), nums.end());
-        for (int i = 0; i < n; i++) {
-            if ( i>0 && nums[i] == nums[i - 1])
-                continue;
-            l = i + 1;
-            r = n - 1;
-
-            while (l < r ) {
-
-                tsum = nums[i] + nums[l] + nums[r];
-                if (tsum > 0) {
-                    r--;
-                } else if (tsum < 0) {
-                    l++;
-                } else {
-                    answer.push_back({nums[i], nums[l], nums[r]});
-                    l++;
-                    r--;
-                
-                while (l < r && l>0 && nums[l] == nums[l - 1] ) {
-                    l++;
+        vector <vector <int>> ans;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0; i<n-2; i++){
+            if(i>0 && nums[i-1]==nums[i]) continue;
+            int j=i+1,k=n-1;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum==0){
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    j++;
+                    k--;
+                    while(j>i+1 && j<k && nums[j]==nums[j-1]) j++;
+                    while(k>j && k< n-1  && nums[k]==nums[k+1] )k--;
+                } 
+                else if(sum>0){
+                    k--;
                 }
-                while (l < r && r<n-1 && nums[r] == nums[r + 1]) {
-                    r--;
-                }
+                else j++;
             }
-            }
+
+
         }
-        // set<vector<int>> st(answer.begin(), answer.end());
-        // answer.assign(st.begin(), st.end());
-        return answer;
+        return ans;
+        
     }
 };
