8b3bcbf203655729ad7495b400b5a1fb05bcae2c
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-12-24 01:03:51 +0530

    LC-sum-of-square-numbers [Runtime: 0 ms; Memory: 7.6 MB]

--- two-pointers/sum-of-square-numbers.cpp
+++ two-pointers/sum-of-square-numbers.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
-       int l=0,r=sqrt(c);
+       long long  l=0;
+       long long r= (long long )sqrt  (c);
        while(l<=r){
+   
         long long sum=l*l+r*r;
         if(sum==c) return true;
         else if(sum<c)l++;
