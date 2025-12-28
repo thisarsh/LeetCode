86472e8544ac65e0626748fd09161c0f26ad5c96
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-12-28 23:52:25 +0530

    LC-maximum-depth-of-binary-tree [Runtime: N/A; Memory: N/A]

---
+++ tree/maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int depth(TreeNode *root,int count){
+        if(!root)return count;
+        if(!root->left && !root->right)return count;
+        if(root->left){
+            count++;
+            depth(root->left,count);
+        }
+        if(root->right){
+            count++;
+            depth(root->left,count);
+        }
+
+        return count+1;
+
+
+        
+
+    }
+public:
+    int maxDepth(TreeNode* root) {
+        return depth(root,0);
+        
+    }
+};
