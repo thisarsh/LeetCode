8632e1bc7259bd0e20de5f9b7c1821389b34cc04
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2026-01-04 11:17:25 +0530

    LC-four-divisors [Runtime: N/A; Memory: N/A]

---
+++ array/four-divisors.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+int  checker(int n){
+    int count=0,sum=0;
+    for(int i=1; i<=n; i++){
+        if(n%i==0){
+            count++;
+            sum+=i;
+        }
+      
+
+    }
+    if(count==4)return sum;
+    return 0;
+}
+    int sumFourDivisors(vector<int>& nums) {
+        int n=nums.size();
+        int ans=0;
+        for(auto i:nums){
+            ans+=checker(i);
+        }
+        return ans;
+
+        
+    }
+};
