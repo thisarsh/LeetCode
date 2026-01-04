0d3cbba4e0cb0617a66c8972ebc20cf12ca17f0b
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2026-01-04 11:46:48 +0530

    LC-four-divisors [Runtime: 54 ms; Memory: 16.3 MB]

--- math/four-divisors.cpp
+++ math/four-divisors.cpp
@@ -2,14 +2,21 @@ class Solution {
 public:
 int checker(int n){
     int count = 0, sum = 0;
-    for(int i = 1; i <= (n); i++){
-        if(n % i == 0 ){
-            count++;
-            sum += i;
+    for(int i = 1; i <= sqrt(n); i++){
+        if(n % i == 0){
+            if(i == n / i){          // FIX: perfect square
+                count++;
+                sum += i;
+            } else {
+                count += 2;
+                sum += i;
+                sum += n / i;
+            }
         }
     }
     if(count == 4) return sum;
     return 0;
+
 }
 
     int sumFourDivisors(vector<int>& nums) {
