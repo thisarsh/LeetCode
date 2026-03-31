d7d482e5a2f6236c2377adc734f1da03f052f4da
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2026-03-31 15:44:22 +0530

    LC-3sum [Runtime: 47 ms; Memory: 29.1 MB]

--- two-pointers/3sum.cpp
+++ two-pointers/3sum.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector <vector <int>> ans;
         int n=nums.size();
         sort(nums.begin(),nums.end());
+        int sum=0;
+        vector <vector <int>> ans;
         for(int i=0; i<n-2; i++){
-            if(i>0 && nums[i-1]==nums[i]) continue;
+            if(i> 0 && nums[i-1]==nums[i])continue;
             int j=i+1,k=n-1;
             while(j<k){
-                int sum=nums[i]+nums[j]+nums[k];
+                sum=nums[i]+nums[j]+nums[k];
                 if(sum==0){
                     ans.push_back({nums[i],nums[j],nums[k]});
                     j++;
                     k--;
-                    while(j>i+1 && j<k && nums[j]==nums[j-1]) j++;
-                    while(k>j && k< n-1  && nums[k]==nums[k+1] )k--;
-                } 
+                   while(j<k && nums[j]==nums[j-1]) j++;
+                    while(k>j && nums[k]==nums[k+1])k--;
+                }
                 else if(sum>0){
                     k--;
                 }
                 else j++;
+
+                
             }
-
-
         }
         return ans;
+        
+
+
         
     }
 };
