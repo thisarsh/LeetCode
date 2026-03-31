cabee8f39f3181772c5ebc13534d01035a0afd21
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2026-04-01 03:47:03 +0530

    LC-remove-duplicates-from-sorted-array [Runtime: 0 ms; Memory: 22.6 MB]

--- two-pointers/remove-duplicates-from-sorted-array.cpp
+++ two-pointers/remove-duplicates-from-sorted-array.cpp
@@ -2,15 +2,11 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int n=nums.size(),j=0;
-        for(int i=1; i<n; i++){
-            if(nums[i]!=nums[j]) {
-                nums[j+1]=nums[i];
-                j++;
-                
-            }
-            
-            
+        for(int i=0; i<n; i++){
+            if(nums[i]!=nums[j])(nums[++j]=nums[i]);
         }
         return j+1;
+
+        
     }
 };
