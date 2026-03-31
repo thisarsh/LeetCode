b125cb0054d0122f41f4fda1dc9008ad33981ff3
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2026-04-01 02:13:29 +0530

    LC-merge-intervals [Runtime: N/A; Memory: N/A]

--- sorting/merge-intervals.cpp
+++ sorting/merge-intervals.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector < vector <int>> ans;
+        int n=intervals.size();
+        if(n==1)return intervals;
         sort(intervals.begin(),intervals.end());
-        vector < vector <int> > ans;
-        ans.push_back({intervals[0][0],intervals[0][1]});
-        for(auto &it:intervals){
-         if(ans.back()[1]>=it[0] && ans.back()[1]<it[1]){
-            ans.back()[1]=it[1];
-         }
-         else if(ans.back()[1]<it[0])
-         ans.push_back({it[0],it[1]}) ;
+        ans.push_back(intervals.front());
+        for(int i=1; i<n; i++){
+            if(intervals[i][0]<=ans.back()[1])
+            ans.back()[1]=intervals[i][1];
+            else ans.push_back({intervals[i][0],intervals[i][1]});
         }
         return ans;
     }
