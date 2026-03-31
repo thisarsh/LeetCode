de1cb71dac128310a93d2f2c1b072b894ce085b0
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2026-04-01 02:23:19 +0530

    LC-merge-intervals [Runtime: 7 ms; Memory: 23.9 MB]

--- array/merge-intervals.cpp
+++ array/merge-intervals.cpp
@@ -7,8 +7,11 @@ public:
         sort(intervals.begin(),intervals.end());
         ans.push_back(intervals.front());
         for(int i=1; i<n; i++){
-            if(intervals[i][0]<=ans.back()[1])
+              if(intervals[i][1]<ans.back()[1] )continue;
+           else  if(intervals[i][0]<=ans.back()[1])
             ans.back()[1]=intervals[i][1];
+          
+
             else ans.push_back({intervals[i][0],intervals[i][1]});
         }
         return ans;
