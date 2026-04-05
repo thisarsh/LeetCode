ff1ac9ce15414ea9d7b37ade1a39d50817730ff8
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2026-04-06 01:06:53 +0530

    LC-merge-sorted-array [Runtime: 0 ms; Memory: 12.4 MB]

--- array/merge-sorted-array.cpp
+++ array/merge-sorted-array.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-      int p3=m+n-1;
-      int p1=m-1;
-      int p2=n-1;
-        while(p1>=0 && p2>=0){
-            if(nums2[p2]>nums1[p1]){
-                nums1[p3--]=nums2[p2--];
+        int n1=nums1.size();
+        int n2=nums2.size();
+        int i=m-1;
+        int j=n-1;
+        int k=n1-1;
+        while(i>=0 && j>=0){
+            if(nums1[i]<nums2[j]){
+                nums1[k--]=nums2[j--];
             }
             else{
-                
-                nums1[p3--]=nums1[p1--];
+                nums1[k--]=nums1[i--];
             }
-        }
-        while(p2>=0){
-            nums1[p3--]=nums2[p2--];
 
         }
-
-        // return nums1;
+        while(i>=0){
+            nums1[k--]=nums1[i--];
+        }
+        while(j>=0){
+            nums1[k--]=nums2[j--];
+        }
 
     }
 };
