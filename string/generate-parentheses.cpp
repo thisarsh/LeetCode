17908986f6478f7cdfc15b36281ed3a684355a7e
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2026-06-02 09:00:24 +0530

    LC-generate-parentheses [Runtime: 3 ms; Memory: 13 MB]

--- string/generate-parentheses.cpp
+++ string/generate-parentheses.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-    void f(vector <string> & ans, string &s, int open,int close, int n){
+
+    void f(vector<string> &ans,string &s,int open, int close,int n ) {
         if(s.size()==2*n){
             ans.push_back(s);
+            // f()
             return;
         }
-        if( open <n){
+        if(open<n){
             s+='(';
             f(ans,s,open+1,close,n);
             s.pop_back();
-
+            
         }
-        if(close<open){
+        if(open>close){
             s+=')';
             f(ans,s,open,close+1,n);
             s.pop_back();
         }
+        
     }
     vector<string> generateParenthesis(int n) {
         vector <string> ans;
-        string s;
+        string s="";
         f(ans,s,0,0,n);
         return ans;
     }
