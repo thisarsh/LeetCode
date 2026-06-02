c299316029e5b5384e55f8f0e36ed2c3d4e8be9a
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2026-06-03 02:03:14 +0530

    LC-combination-sum-ii [Runtime: N/A; Memory: N/A]

--- array/combination-sum-ii.cpp
+++ array/combination-sum-ii.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-    void dfs( vector <int> &candidates,vector <vector <int>> &ans,
-        vector <int> &temp, int target, int i,int sum){
-        if(sum>=target ||i>=candidates.size()) {
-            if(sum==target)ans.push_back(temp);
-            return;
-        }
-        temp.push_back(candidates[i]);
-        dfs(candidates,ans,temp,target,i+1,sum+candidates[i]);
-        
-         temp.pop_back();
-        while(i<candidates.size()-1 && candidates[i]==candidates[i+1]) i++;
-       
-        dfs(candidates,ans,temp,target,i+1,sum);
-        
-
-
+ void f(vector <int> &candidates,vector <vector <int >> &ans, int sum, int i, vector <int>
+  &curr,int target){
+    if(sum==target){
+        ans.push_back(curr);
+        return ;
     }
+    if(i==candidates.size() || sum >target)return;
+    
+        curr.push_back(candidates[i]);
+        f(candidates,ans,sum+candidates[i],i+1,curr,target);
+        curr.pop_back();
+        while(i<candidates.size()-1 && candidates[i]==candidates[i+1])i++;
+        f(candidates,ans,sum,i+1,curr,target);
+
+    // 112567,10
+
+
+
+ }
+
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         vector <vector <int>> ans;
-        vector <int> temp;
+        vector <int> curr;
         sort(candidates.begin(),candidates.end());
-        dfs(candidates,ans,temp,target,0,0);
+        f(candidates,ans,0,0,curr,target);
         return ans;
-
     }
 };
