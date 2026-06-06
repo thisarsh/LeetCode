cf8adeaf2b42f346e6e31fe383375d963501fb49
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2026-06-06 13:42:39 +0530

    LC-symmetric-tree [Runtime: N/A; Memory: N/A]

--- tree/symmetric-tree.cpp
+++ tree/symmetric-tree.cpp
@@ -1,15 +1,29 @@
-bool f(TreeNode *r1, TreeNode *r2){
-    if(!r1 && !r2 ) return 1;
-    if(!r1 || !r2) return 0;
-    if(r1->val !=r2->val) return 0;
-    if(r1->val !=r2->val) return 0;
-    return f(r1->left ,r2->right) && f(r1->right ,r2->left);
- }
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
+    bool dfs(TreeNode *l, TreeNode*r){
+        if(!l && !r) return 1;
+        if(!l || !r)return 0; 
+        if(l->val!=r->val)return 0;
+       if( dfs(l->right,r->left) && dfs(r->left,l->right)) return 1;
+       return 0;
+
+
+    }
     bool isSymmetric(TreeNode* root) {
-        if(!root) return 0;
-        TreeNode *r1=root,*r2=root;
-        return f(r1,r2);
+        if(!root->left || ! root->right)return 0;
+        return dfs(root->left, root->right);
+
+        
     }
 };
